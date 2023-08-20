aabe35737961a8b456f5e305acdeaf1bf3f4ced7
sudddy <34531983+sudddy@users.noreply.github.com>
2023-08-19 20:21:53 -0700

    Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors : {
+        'mvml-base' : '#eef1fe',
+        'custom-color': 'rgb(238, 241, 254)'
+      },
+      transformOrigin: ['responsive', 'hover', 'focus'],
+      keyframes: {
+        'moving-line': {
+            from: {
+                bottom: '0%',
+                opacity: '0',
+            },
+            to: {
+                bottom: '13%',
+                opacity: '100',
+            },
+        },
+    },
+    animation: {
+        'moving-line': 'moving-line 1s ease-in-out forwards',
+    },
+    },
+  },
+  plugins: [],
+};
